84c2babc84936826f1a853fc958b55b4cdbc9541
bjuanyshev <bjuanyshev@greet-go.com>
2020-11-02 10:35:40 +0600

    Создал проект для изучения кафки

---
+++ src/test/java/kz/kafka/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kafka.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
